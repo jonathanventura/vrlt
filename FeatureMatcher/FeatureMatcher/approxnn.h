0ea5c01dfb78fe441bc8b754bac4d4b15fc96766
Jonathan Ventura <jonathan.d.ventura@gmail.com>
2014-04-21 16:46:07 +0200

    Adding approx NN with FLANN

---
+++ FeatureMatcher/FeatureMatcher/approxnn.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014. Jonathan Ventura
+ * Licensed pursuant to the terms and conditions available for viewing at:
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * File: approxnn.h
+ * Author: Jonathan Ventura
+ * Last Modified: 04.21.2014
+ */
+
+#ifndef BRUTE_FORCE_NN_H
+#define BRUTE_FORCE_NN_H
+
+#include "NN.h"
+
+#include <opencv2/features2d.hpp>
+#include <opencv2/flann.hpp>
+
+namespace vrlt {
+/**
+ * \addtogroup FeatureMatcher
+ * @{
+ */
+
+    class ApproxNN : public NN
+    {
+    public:
+        int N;
+        unsigned char *data;
+        float *float_data;
+        
+        ApproxNN();
+        ~ApproxNN();
+        
+        virtual void setData( int _N, unsigned char *_data );
+        
+        void findnn( int num_queries, unsigned char *queries, int *neighbors, unsigned int *distances_sq );
+        void findknn( int num_queries, unsigned char *queries, int k, int *neighbors, unsigned int *distances_sq );
+    protected:
+        cv::flann::GenericIndex< cv::flann::L2<unsigned char> > *flannindex;
+    };
+
+/**
+ * @}
+ */
+}
+
+#endif
---
+++ FeatureMatcher/src/approxnn.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2014. Jonathan Ventura
+ * Licensed pursuant to the terms and conditions available for viewing at:
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * File: approxnn.cpp
+ * Author: Jonathan Ventura
+ * Last Modified: 04.21.2014
+ */
+
+#include <FeatureMatcher/approxnn.h>
+
+#include <cstdio>
+#include <cstdlib>
+
+namespace vrlt {
+    
+    ApproxNN::ApproxNN() : N(0), data(NULL), flannindex(NULL)
+    {
+        
+    }
+    
+    void ApproxNN::setData( int _N, unsigned char *_data )
+    {
+        N = _N;
+        data = _data;
+        
+        cv::Mat trainDescriptors( N, 128, CV_8UC1, data );
+     
+        ::cvflann::KDTreeIndexParams params;
+        
+        delete flannindex;
+        flannindex = new cv::flann::GenericIndex< cv::flann::L2<unsigned char> >( trainDescriptors, params );
+        
+        std::cout << "veclen: " << flannindex->veclen() << "\n";
+    }
+    
+    ApproxNN::~ApproxNN()
+    {
+        delete flannindex;
+    }
+    
+    void ApproxNN::findnn( int num_queries, unsigned char *queries, int *neighbors, unsigned int *distances_sq )
+    {
+        ::cvflann::SearchParams params;
+        
+        cv::Mat queryDescriptors( num_queries, 128, CV_8UC1, queries );
+        cv::Mat indices( num_queries, 1, CV_32SC1, neighbors );
+        cv::Mat dists( num_queries, 1, CV_32FC1 );
+        flannindex->knnSearch( queryDescriptors, indices, dists, 1, params );
+        
+        unsigned int *distances_ptr = distances_sq;
+        for ( int m = 0; m < num_queries; m++,distances_ptr++ )
+        {
+            *distances_ptr = (unsigned int)dists.at<float>(m);
+        }
+    }
+    
+    void ApproxNN::findknn( int num_queries, unsigned char *queries, int k, int *neighbors, unsigned int *distances_sq )
+    {
+        ::cvflann::SearchParams params;
+        
+        cv::Mat queryDescriptors( num_queries, 128, CV_8UC1, queries );
+        cv::Mat indices( num_queries, k, CV_32SC1, neighbors );
+        cv::Mat dists( num_queries, k, CV_32FC1 );
+        flannindex->knnSearch( queryDescriptors, indices, dists, k, params );
+        
+        unsigned int *distances_ptr = distances_sq;
+        for ( int m = 0; m < num_queries; m++ )
+        {
+            for ( int j = 0; j < k; j++,distances_ptr++ )
+            {
+                *distances_ptr = (unsigned int)dists.at<float>(m,j);
+            }
+        }
+    }
+    
+}
