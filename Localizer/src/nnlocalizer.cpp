b18c81f01f8c424169d5c0f8843a7e70c1c9e7aa
Jonathan Ventura <ventura@icg.tugraz.at>
2014-05-02 07:49:15 +0200

    got nnlocalizer working

--- Localizer/src/nnlocalizer.cpp
+++ Localizer/src/nnlocalizer.cpp
@@ -20,14 +20,12 @@
 
 #include <iostream>
 
-static const int min_level = 0;
-
 namespace vrlt
 {
     NNLocalizer::NNLocalizer( Node *_root, NN *index ) : Localizer( _root )
     {
         fm = new FeatureMatcher( index );
-        fm->init( _root, true, true );
+        fm->init( _root, true, false );
     }
     
     NNLocalizer::~NNLocalizer()
@@ -37,9 +35,8 @@ namespace vrlt
     
     static void drawMatches( Node *querynode, std::vector<Match*> &matches, std::vector<bool> &inliers )
     {
-        /*
         std::vector<Camera*> cameras;
-        std::vector< cv::Mat* > images;
+        std::vector<cv::Mat> images;
         cv::Mat queryim = querynode->camera->image;
         
         for ( int j = 0; j < matches.size(); j++ )
@@ -54,7 +51,7 @@ namespace vrlt
             Feature *queryfeature = match->feature2;
             
             bool found = false;
-            Image<byte> *image = NULL;
+            cv::Mat image;
             for ( int k = 0; k < cameras.size(); k++ ) {
                 if ( feature->camera == cameras[k] ) {
                     found = true;
@@ -62,34 +59,28 @@ namespace vrlt
                     break;
                 }
             }
-            if ( image == NULL ) {
+            if ( image.empty() ) {
                 cameras.push_back( feature->camera );
-                Image<byte> im = feature->camera->image;
-                int height = ( queryim.size().y > im.size().y ) ? queryim.size().y : im.size().y;
-                Image<byte> *composite = new Image<byte>( ImageRef( im.size().x+queryim.size().x, height ) );
-                composite->sub_image( ImageRef(0,0), queryim.size() ).copy_from( queryim );
-                composite->sub_image( ImageRef(queryim.size().x,0), im.size() ).copy_from( im );
+                cv::Mat im = feature->camera->image;
+                int height = ( queryim.size().height > im.size().height ) ? queryim.size().height : im.size().height;
+                cv::Mat composite( cv::Size( im.size().width+queryim.size().width, height ), CV_8UC1 );
+                queryim.copyTo( composite( cv::Rect(cv::Point2i(0,0), queryim.size() ) ) );
+                im.copyTo( composite( cv::Rect(cv::Point2i(queryim.size().width,0), im.size() ) ) );
                 images.push_back( composite );
                 image = images.back();
             }
             
-            ImageRef impt1( queryfeature->location[0], queryfeature->location[1] );
-            ImageRef impt2( queryim.size().x+feature->location[0], feature->location[1] );
+            cv::Point2i impt1( queryfeature->location[0], queryfeature->location[1] );
+            cv::Point2i impt2( queryim.size().width+feature->location[0], feature->location[1] );
             
-            drawCross( *image, impt1, queryfeature->scale, (byte)0 );
-            drawCross( *image, impt2, feature->scale, (byte)0 );
-            
-            drawLine( *image, impt1, impt2, (byte)0 );
-            
+            cv::line( image, impt1, impt2, cv::Scalar(0) );
         }
         
         for ( int j = 0; j < cameras.size(); j++ ) {
             char name[256];
             sprintf( name, "Output/%s_%s_matches.jpg", querynode->name.c_str(), cameras[j]->name.c_str() );
-            img_save( *(images[j]), name, ImageType::JPEG );
-            delete images[j];
+            cv::imwrite( name, images[j] );
         }
-        */
     }
     
     struct SortByFeatureName
@@ -105,19 +96,11 @@ namespace vrlt
         
         std::vector<Match*> matches;
         
-        findMatches( (*fm), features, matches );
+        //findMatches( (*fm), features, matches );
+        findUniqueMatches( (*fm), features, 0.8, matches );
 
         std::vector<bool> inliers;
-
-        std::map<std::string,float> scores;
-        for ( int i = 0; i < matches.size(); i++ )
-        {
-            std::string trackname = matches[i]->feature1->track->name;
-            if ( scores.count(trackname) == 0 ) scores[trackname] = matches[i]->score;
-            else if ( matches[i]->score < scores[trackname] ) scores[trackname] = matches[i]->score;
-        }
         
-        PointPairList all_point_pairs;
         PointPairList point_pairs;
         for ( int i = 0; i < matches.size(); i++ )
         {
@@ -126,18 +109,14 @@ namespace vrlt
             Feature *feature = match->feature1;
             Feature *queryfeature = match->feature2;
             
-            if ( match->score != scores[feature->track->name] ) continue;
-            
             PointPair point_pair;
             point_pair.first = project( feature->track->point->position );
             point_pair.second = queryfeature->unproject();
             
-            int querylevel = (int) log2f( queryfeature->scale / 1.6 );
-            if ( querylevel >= min_level ) point_pairs.push_back( point_pair );
-            all_point_pairs.push_back( point_pair );
+            point_pairs.push_back( point_pair );
         }
         
-        std::cout << point_pairs.size() << " matches for pose estimation; " << all_point_pairs.size() << " matches for inliers\n";
+        std::cout << point_pairs.size() << " matches for pose estimation\n";
         
         ThreePointPose estimator;
         
@@ -145,11 +124,13 @@ namespace vrlt
         prosac.num_trials = 5000;
         prosac.min_num_inliers = 100;
         prosac.inlier_threshold = thresh;
-        
-        int ninliers = prosac.compute( point_pairs.begin(), point_pairs.end(), all_point_pairs.begin(), all_point_pairs.end(), estimator, inliers );
+
+        int ninliers = prosac.compute( point_pairs.begin(), point_pairs.end(), estimator, inliers );
         
         ElementList::iterator it;
         std::cout << matches.size() << " matches; " << ninliers << " PROSAC inliers\n";
+        
+        drawMatches( querycamera->node, matches, inliers );
         
         for ( int i = 0; i < inliers.size(); i++ )
         {
