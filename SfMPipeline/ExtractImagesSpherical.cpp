ee4ae04c563f80ac9afe39e0615c33b58d2152e5
Jonathan Ventura <ventura@icg.tugraz.at>
2014-05-16 11:31:33 +0200

    Changed size in ExtractImagesSpherical

--- SfMPipeline/ExtractImagesSpherical.cpp
+++ SfMPipeline/ExtractImagesSpherical.cpp
@@ -142,7 +142,7 @@ int main( int argc, char **argv )
     int base_height = 768;
     
 //    cv::Size sz( base_height * .6, base_height );
-    cv::Size sz( base_height, base_height * .6 );
+    cv::Size sz( base_height, base_height );//* .6 );
     
     Calibration *calibration = new Calibration;
     calibration->name = "calibration";
